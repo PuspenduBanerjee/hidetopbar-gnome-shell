dbd4099f25289575b097e4ddc2568a343a8d3249
puspendu banerjee <puspendu.banerjee@gmail.com>
2024-03-12 18:52:52 -0500

    Gnome Shell 46 Compat fix. global.backend instead  of global.display .

--- panelVisibilityManager.js
+++ panelVisibilityManager.js
@@ -314,7 +314,8 @@ export var PanelVisibilityManager = class HideTopBar_PanelVisibilityManager{
             direction = Meta.BarrierDirection.NEGATIVE_Y;
         }
         this._panelBarrier = new Meta.Barrier({
-            display: global.display,
+            // display: global.display,
+            backend: global.backend,
             x1: PanelBox.x,
             x2: PanelBox.x + PanelBox.width,
             y1: this._base_y - anchor_y,
